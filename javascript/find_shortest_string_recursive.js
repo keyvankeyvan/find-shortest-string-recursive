79cb668b88d13150328f8e0f5c959e349d7d9c39
Keyvan Alonso <keyvanalonso@gmail.com>
2022-05-31 16:38:11 +0200

    completed lab

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,9 +1,39 @@
 function findShortestStringRecursive(arr) {
   // type your code here
+  if (arr[1] == undefined){
+    return arr[0]
+  }
+
+  if (arr[0].length <= arr[1].length ){
+    arr.splice(1,1)
+    findShortestStringRecursive(arr)
+  } else{
+    arr.splice(0,1)
+    findShortestStringRecursive(arr)
+  }
+  return arr[0]
 }
 
+function arrMaker(){
+  let arr = []
+  let str = 'a'
+  for(let i = 0; i < 50; i++){
+    arr.push(str.repeat(Math.floor(20 * (Math.random(0)))))
+  }
+  return arr
+}
+
+
 if (require.main === module) {
-  // add your own tests in here
+  let myarr = arrMaker()
+
+  startTime = Date.now()
+  for(let i = 0; i < 10000; i++){
+    arrcpy =[...myarr]
+    findShortestStringRecursive(arrcpy)
+  }
+  console.log(Date.now() - startTime)
+
   console.log("Expecting: 'a'");
   console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
 
@@ -20,5 +50,3 @@ if (require.main === module) {
 
 module.exports = findShortestStringRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
